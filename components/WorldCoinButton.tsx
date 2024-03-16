fd4a549c544c8fc6993a9afdc6117933b8524ab2
Nico <nicobdx@icloud.com>
2024-03-16 12:39:49 +0000

    add worlcoind verify badge

--- components/WorldCoinButton.tsx
+++ components/WorldCoinButton.tsx
@@ -24,6 +24,7 @@ export default function WorldcoinButton() {
   };
 
   useEffect(() => {
+    handleVerify();
     setIsClient(true);
     const isUserVerified = localStorage.getItem("worldcoin-verified");
     setUserVerified(isUserVerified === "true");
--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,12 +1,14 @@
 import { Network } from "@/lib/types";
 import { Atom, GlobeLock, Receipt } from "lucide-react";
 import { UseAccountReturnType } from "wagmi";
+import Image from "next/image";
 
 const ProfileScreen = (params: {
   profileAddress: string;
-  identity: string | string[] | undefined;
+  identity: string;
   network: Network;
   userAccount: UseAccountReturnType;
+  worldCoinVerified: boolean;
 }) => {
   const { identity, network } = params;
 
@@ -15,28 +17,36 @@ const ProfileScreen = (params: {
   return (
     <div className="w-full flex justify-center mb-16">
       <div className="w-6/12 flex flex-col">
-        <h1 className="mb-10 text-3xl font-bold text-center glass p-5">{identity}</h1>
+        <h1 className={`mb-10 font-bold text-center glass p-5 ${identity?.length > 10 ? "text-xl" : "text-3xl"}`}>{identity}</h1>
 
         <div className="flex justify-evenly text-center">
           <div className="flex flex-col justify-center items-center">
             <h1 className="text-xl font-bold ">
+              <p className="text-gray-500 font-bold mb-2">NETWORK VALUE</p>
               <Atom className="mx-auto mb-3" size={50} />${network.value}
             </h1>
-            <p className="text-blue-500">Network Value</p>
           </div>
           <div className="flex flex-col justify-center items-center">
             <h1 className="text-xl font-bold">
+              <p className="text-gray-500 font-bold mb-2">NETWORK SIZE</p>
               <GlobeLock className="mx-auto mb-3" size={50} />
               {network.links}
             </h1>
-            <p className="text-blue-500">Network Size</p>
           </div>
           <div className="flex flex-col justify-center items-center">
             <h1 className="text-xl font-bold ">
+              <p className="text-gray-500 font-bold mb-2">USERS&apos;S APPROVALS</p>
               <Receipt className="mx-auto mb-3" size={50} />${network.approved}
             </h1>
-            <p className="text-blue-500">User&apos;s Approval</p>
           </div>
+          {params.worldCoinVerified && (
+            <div className="flex flex-col justify-start items-center">
+              <h1 className="text-xl font-bold flex flex-col items-center ">
+                <p className="text-gray-500 font-bold mb-2">WORLDCOIN VERIFIED</p>
+                <Image className="w-16 h-16" src="/worldcoin_verified.png" alt="worldcoin-verified" width={50} height={50} />
+              </h1>
+            </div>
+          )}
         </div>
       </div>
     </div>
