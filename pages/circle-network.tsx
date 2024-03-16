b811954bedce46adce2359336bf7528de3fd4da5
Anthony ISSA <anthony.issa@hotmail.fr>
2024-03-16 23:20:03 +0100

    before zbeul

--- pages/circle-network.tsx
+++ pages/circle-network.tsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-
+//
 const CircleNetwork: NextPage = () => {
   const network = [
     {
